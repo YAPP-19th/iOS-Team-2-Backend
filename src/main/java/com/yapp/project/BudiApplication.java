01aed2d834a69d8caaa4e580c42997d63dc24253
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-07 18:21:31 +0900

    chore: 작은 수정

--- src/main/java/com/yapp/project/BudiApplication.java
+++ src/main/java/com/yapp/project/BudiApplication.java
@@ -1,7 +1,6 @@
 package com.yapp.project;
 
 import com.yapp.project.config.ApplicationConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
