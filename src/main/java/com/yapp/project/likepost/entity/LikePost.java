be91220bcb0b70747702ab5e467c77c52d34655c
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-08 05:18:03 +0900

    fix: IDENTITY -> AUTO

--- src/main/java/com/yapp/project/likepost/entity/LikePost.java
+++ src/main/java/com/yapp/project/likepost/entity/LikePost.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class LikePost {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "like_post_id", unique = true)
     private Long id;
 
