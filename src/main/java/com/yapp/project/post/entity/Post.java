83ef681717686485bc9e1815b52498b7535a2630
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-07 18:52:17 +0900

    chore: 불필요한 import 문 제거

--- src/main/java/com/yapp/project/post/entity/Post.java
+++ src/main/java/com/yapp/project/post/entity/Post.java
@@ -10,8 +10,6 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
