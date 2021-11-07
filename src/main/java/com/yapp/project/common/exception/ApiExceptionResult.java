464552196f47927ec4d29a8f1eb9c330bd5cdb45
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-07 18:22:16 +0900

    fix: 응답 server time 형식 변경

--- src/main/java/com/yapp/project/common/exception/ApiExceptionResult.java
+++ src/main/java/com/yapp/project/common/exception/ApiExceptionResult.java
@@ -10,7 +10,7 @@ public class ApiExceptionResult {
     private final int statusCode;
     private final String message;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private final LocalDateTime serverDateTime;
 
     private ApiExceptionResult(int statusCode, String message) {
