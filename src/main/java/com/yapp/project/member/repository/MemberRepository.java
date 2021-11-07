e64aa0a49d2f7bdbdce3948a6785c09b892ef692
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-07 18:50:55 +0900

    feat: 사용한 메소드 명시

--- src/main/java/com/yapp/project/member/repository/MemberRepository.java
+++ src/main/java/com/yapp/project/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.yapp.project.member.repository;
 import com.yapp.project.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Override
+    Optional<Member> findById(Long aLong);
 }
