2777f7a207b05ec66fefe63a2ddd8eb512cd6bf9
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-08 16:29:32 +0900

    chore: swagger config 변경

--- src/main/java/com/yapp/project/config/SwaggerConfig.java
+++ src/main/java/com/yapp/project/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.yapp.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -26,7 +28,7 @@ public class SwaggerConfig {
             "BUDI REST API",
             "BUDI back-end REST API Service",
             "1.0",
-            "urn:tos",
+            "https://github.com/YAPP-19th/iOS-Team-2-Backend",
             DEFAULT_CONTACT,
             "Apache 2.0",
             "http://www.apache.org/licenses/LICENSE-2.0",
@@ -34,7 +36,7 @@ public class SwaggerConfig {
     );
 
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
-            Arrays.asList("application/json", "application/xml")
+            Arrays.asList("application/json", "application/xml", "application/hal+json")
     );
 
     @Bean
@@ -42,6 +44,10 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(Default_API_INFO)
                 .produces(DEFAULT_PRODUCES_AND_CONSUMES)
-                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
     }
 }
