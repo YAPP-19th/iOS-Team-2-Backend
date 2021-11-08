47443ad0a6688a07b0e6b4d10bf97c91ab72d56c
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-08 16:39:36 +0900

    feat: avoid dto passing between controller and service

---
+++ src/main/java/com/yapp/project/post/controller/bundle/RecruitingPositionBundle.java
@@ -0,0 +1,14 @@
+package com.yapp.project.post.controller.bundle;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RecruitingPositionBundle {
+    private int positionCode;
+
+    private int skillCode;
+
+    private int recruitingNumber;
+}
