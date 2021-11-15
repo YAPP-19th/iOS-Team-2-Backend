1e7ef3ac66d31621c5f35f5c4432c370818fe6e0
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-15 17:34:44 +0900

    refactor: position nmae, skill name 추가

--- src/main/java/com/yapp/project/common/vo/Position.java
+++ src/main/java/com/yapp/project/common/vo/Position.java
@@ -9,37 +9,39 @@ import java.util.Arrays;
 @Getter
 public enum Position {
     // default position //
-    DEVELOPER_DEFAULT(1),
-    PLANNER_DEFAULT(2),
-    DESIGNER_DEFAULT(3),
+    DEVELOPER_DEFAULT(1, "개발 기타"),
+    PLANNER_DEFAULT(2, "디자인 기타"),
+    DESIGNER_DEFAULT(3, "기획 기타"),
 
     // developer segmentation //
-    DEVELOPER_HYBRID(4),
-    DEVELOPER_IOS(5),
-    DEVELOPER_AOS(6),
-    DEVELOPER_WEBFRONT(7),
-    DEVELOPER_WEBBACK(8),
-    DEVELOPER_BLOCKCHAIN(9),
-    DEVELOPER_AI(10),
+    DEVELOPER_HYBRID(4, "하이브리드 개발"),
+    DEVELOPER_IOS(5, "iOS 개발"),
+    DEVELOPER_AOS(6, "AOS 개발"),
+    DEVELOPER_WEBFRONT(7, "웹 프론트 개발"),
+    DEVELOPER_WEBBACK(8, "웹 백엔드 개발"),
+    DEVELOPER_BLOCKCHAIN(9, "블록체인 개발"),
+    DEVELOPER_AI(10, "AI 개발"),
 
     // planner segmentation //
-    PLANNER_BUSINESS(11),
-    PLANNER_UIUX(12),
-    PLANNER_PROJECTMANAGER(13),
-    PLANNER_PRODUCTPLANNER(14),
-    PLANNER_SERVICEOPERATOR(15),
+    PLANNER_BUSINESS(11, "비즈니스 기획"),
+    PLANNER_UIUX(12, "UI/UX 기획"),
+    PLANNER_PROJECTMANAGER(13, "PM"),
+    PLANNER_PRODUCTPLANNER(14, "상품 기획"),
+    PLANNER_SERVICEOPERATOR(15, "서비스 기획"),
 
     // designer segmentation //
-    DESIGNER_UIUX(16),
-    DESIGNER_WEB(17),
-    DESIGNER_MOBILE(18),
-    DESIGNER_ILLUSTRATOR(19),
-    DESIGNER_UX(20);
+    DESIGNER_UIUX(16, "UI/UX 디자인"),
+    DESIGNER_WEB(17, "웹 디자인"),
+    DESIGNER_MOBILE(18, "모바일 디자인"),
+    DESIGNER_ILLUSTRATOR(19, "일러스트레이터"),
+    DESIGNER_UX(20, "UX 디자인");
 
     private final int positionCode;
+    private final String positionName;
 
-    Position(int positionCode) {
+    Position(int positionCode, String positionName) {
         this.positionCode = positionCode;
+        this.positionName = positionName;
     }
 
     public static Position of(int positionCode) {
@@ -47,5 +49,12 @@ public enum Position {
                 .filter(v -> v.positionCode == positionCode)
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException(ExceptionMessage.NOT_EXIST_POSITION_CODE));
+    }
+
+    public static Position of(String positionName) {
+        return Arrays.stream(Position.values())
+                .filter(v -> v.positionName.equals(positionName))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException(ExceptionMessage.NOT_EXIST_POSITION_NAME));
     }
 }
--- src/main/java/com/yapp/project/common/vo/Skill.java
+++ src/main/java/com/yapp/project/common/vo/Skill.java
@@ -9,54 +9,56 @@ import java.util.Arrays;
 @Getter
 public enum Skill {
     // default skill //
-    DEVELOPER_DEFAULT(1),
-    PLANNER_DEFAULT(2),
-    DESIGNER_DEFAULT(3),
+    DEVELOPER_DEFAULT(1, "개발 기타"),
+    PLANNER_DEFAULT(2, "기획 기타"),
+    DESIGNER_DEFAULT(3, "디자인 기타"),
 
     // developer segmentation //
-    DEVELOPER_OBJECTC(4),
-    DEVELOPER_SWIFT(5),
-    DEVELOPER_PYTHON(6),
-    DEVELOPER_RUBY(7),
-    DEVELOPER_JAVA(8),
-    DEVELOPER_KOTLIN(9),
-    DEVELOPER_C(10),
-    DEVELOPER_LUA(11),
-    DEVELOPER_HTML(12),
-    DEVELOPER_CSS(13),
-    DEVELOPER_JAVASCRIPT(14),
-    DEVELOPER_PHP(15),
-    DEVELOPER_GO(16),
-    DEVELOPER_NODE(17),
-    DEVELOPER_SOLIDITY(18),
-    DEVELOPER_SOMPLICITY(19),
-    DEVELOPER_LISP(20),
-    DEVELOPER_R(21),
-    DEVELOPER_PROLOG(22),
-    DEVELOPER_JULIA(23),
+    DEVELOPER_OBJECTC(4, "Object-C"),
+    DEVELOPER_SWIFT(5, "Swift"),
+    DEVELOPER_PYTHON(6, "Python"),
+    DEVELOPER_RUBY(7, "Ruby"),
+    DEVELOPER_JAVA(8, "JAVA"),
+    DEVELOPER_KOTLIN(9, "Kotlin"),
+    DEVELOPER_C(10, "C/C++"),
+    DEVELOPER_LUA(11, "Lua"),
+    DEVELOPER_HTML(12, "HTML"),
+    DEVELOPER_CSS(13, "CSS"),
+    DEVELOPER_JAVASCRIPT(14, "Javascript"),
+    DEVELOPER_PHP(15, "PHP"),
+    DEVELOPER_GO(16, "GO"),
+    DEVELOPER_NODE(17, "NodeJS"),
+    DEVELOPER_SOLIDITY(18, "Solidity"),
+    DEVELOPER_SOMPLICITY(19, "Somplicity"),
+    DEVELOPER_LISP(20, "LISP"),
+    DEVELOPER_R(21, "R"),
+    DEVELOPER_PROLOG(22, "Prolog"),
+    DEVELOPER_JULIA(23, "Julia"),
 
     // planner segmentation //
-    PLANNER_STORYBOARD(24),
-    PLANNER_DOCUMENTATION(25),
-    PLANNER_DATAANALYSIS(26),
-    PLANNER_GA(27),
-    PLANNER_MOBILEPLANNING(28),
-    PLANNER_WEB_PLANNING(29),
-    PLANNER_PUBLISHING(30),
+    PLANNER_STORYBOARD(24, "Storyboard"),
+    PLANNER_DOCUMENTATION(25, "기획 문서화"),
+    PLANNER_DATAANALYSIS(26, "데이터분석 기획"),
+    PLANNER_GA(27, "GA"),
+    PLANNER_MOBILEPLANNING(28, "모바일 기획"),
+    PLANNER_WEB_PLANNING(29, "웹 기획"),
+    PLANNER_PUBLISHING(30, "퍼블리싱"),
 
     // designer segmentation //
-    DESIGNER_ADOBEXD(31),
-    DESIGNER_SKETCH(32),
-    DESIGNER_ZEPLIN(33),
-    DESIGNER_PROTOIO(34),
-    DESIGER_FIGMA(35),
-    DESIGNER_PHOTOSHOP(36),
-    DESIGNER_ILLUSTRATOR(37);
+    DESIGNER_ADOBEXD(31, "어도비XD"),
+    DESIGNER_SKETCH(32, "스케치"),
+    DESIGNER_ZEPLIN(33, "Zeplin"),
+    DESIGNER_PROTOIO(34, "Proto.io"),
+    DESIGNER_FIGMA(35, "Figma"),
+    DESIGNER_PHOTOSHOP(36, "포토샵"),
+    DESIGNER_ILLUSTRATOR(37, "일러스트레이터");
 
     private final int skillCode;
+    private final String skillName;
 
-    Skill(int skillCode) {
+    Skill(int skillCode, String skillName) {
         this.skillCode = skillCode;
+        this.skillName = skillName;
     }
 
     public static Skill of(int skillCode) {
@@ -64,5 +66,12 @@ public enum Skill {
                 .filter(v -> v.skillCode == skillCode)
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException(ExceptionMessage.NOT_EXIST_SKILL_CODE));
+    }
+
+    public static Skill of(String skillName) {
+        return Arrays.stream(Skill.values())
+                .filter(v -> v.skillName.equals(skillName))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException(ExceptionMessage.NOT_EXIST_SKILL_NAME));
     }
 }
