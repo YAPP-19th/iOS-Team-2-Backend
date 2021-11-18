4271fd016c55d4f0708e54d3a8ad36af0ab69624
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-19 03:37:40 +0900

    feat: advice 추가

--- src/main/java/com/yapp/project/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yapp/project/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.yapp.project.common.exception;
 
-import com.yapp.project.common.exception.type.InvalidRequestBodyFieldException;
 import com.yapp.project.common.exception.type.NotFoundException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ApiExceptionResult handleMethodValidException(MethodArgumentNotValidException exception){
-        return createApiExceptionResult(400, new InvalidRequestBodyFieldException(exception.getMessage()));
+        return createApiExceptionResult(4000, exception.getAllErrors().get(0).getDefaultMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
@@ -18,12 +19,35 @@ public class GlobalExceptionHandler {
         return createApiExceptionResult(exception);
     }
 
+    /**
+     * @ModelAttribut 으로 binding error 발생시 BindException 발생한다.
+     * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
+     */
+    @ExceptionHandler(BindException.class)
+    protected ApiExceptionResult handleBindException(BindException exception) {
+        //final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        return createApiExceptionResult(4321, ExceptionMessage.DATA_BINDING_FAIL.name());
+    }
+
+    /**
+     * 지원하지 않은 HTTP method 호출 할 경우 발생
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ApiExceptionResult handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        return createApiExceptionResult(9999, ExceptionMessage.HTTP_REQUEST_METHOD_NOT_SUPPORTED.name());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ApiExceptionResult handleException(Exception exception) {
+        return createApiExceptionResult(0000, ExceptionMessage.DEFAULT_EXCEPTION.name());
+    }
+
     private ApiExceptionResult createApiExceptionResult(Exception exception) {
         ExceptionMessage exceptionMessage = ExceptionMessage.valueOf(exception.getMessage());
         return ApiExceptionResult.of(exceptionMessage);
     }
 
-    private ApiExceptionResult createApiExceptionResult(int httpStatus, Exception exception) {
-        return ApiExceptionResult.of(httpStatus, exception.getMessage());
+    private ApiExceptionResult createApiExceptionResult(int httpStatus, String message) {
+        return ApiExceptionResult.of(httpStatus, message);
     }
 }
