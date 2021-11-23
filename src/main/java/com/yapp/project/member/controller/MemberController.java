6428e689f6560fd479a6fd43797f0080ac7189d6
Yujin Shin <shinnew320@gmail.com>
2021-11-24 00:08:20 +0900

    fix: remove hateoas Member쪽 반영

--- src/main/java/com/yapp/project/member/controller/MemberController.java
+++ src/main/java/com/yapp/project/member/controller/MemberController.java
@@ -3,12 +3,11 @@ package com.yapp.project.member.controller;
 import com.yapp.project.member.service.MemberService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.MediaTypes;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/v1/members", produces = MediaTypes.HAL_JSON_VALUE)
+@RequestMapping(value = "/api/v1/members")
 @Api(tags = "Member")
 public class MemberController {
     private final MemberService memberService;
