6580e3cd0171e100150246e8b571a47b842f84fd
Yujin Shin <43192041+jellyb3ar@users.noreply.github.com>
2021-11-24 00:52:07 +0900

    Feature/#6 oauth (#36)
    * feat: Social Login API By JWT Token
    
    * chore: DTO 파일명 수정
    
    * fix: entity 사용 지양
    
    * fix: remove hateoas Member쪽 반영
    
    * chore: login DTO 수정

--- src/main/java/com/yapp/project/member/dto/LoginRequest.java
+++ src/main/java/com/yapp/project/member/dto/LoginRequest.java
@@ -3,23 +3,20 @@ package com.yapp.project.member.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
 @Getter
 public class LoginRequest {
-    @ApiModelProperty(notes = "기존 발급받은 access Token 존재 시 전달", example = "")
-    private String accessToken;
     @ApiModelProperty(name = "identifier", notes = "소셜로그인에서 받은 identifier 값 전달", example = "17-76025468", required = true)
+    @NotNull
     private String loginId;
-    @ApiModelProperty(name = "email", notes = "소셜 로그인에서 제공받은 값 존재시 전달", example = "test@naver.com")
-    private String email;
-    @ApiModelProperty(name = "name", notes = "소셜 로그인에서 제공받은 값 존재시 전달", example = "별명")
-    private String name;
 
     @Builder
-    public LoginRequest(String accessToken, String loginId, String email, String nickName){
-        this.accessToken = accessToken;
+    public LoginRequest(String loginId){
         this.loginId = loginId;
-        this.email = email;
-        this.name = nickName;
     }
 
 }
--- src/main/java/com/yapp/project/member/service/MemberService.java
+++ src/main/java/com/yapp/project/member/service/MemberService.java
@@ -3,6 +3,7 @@ import com.yapp.project.member.entity.Member;
 import com.yapp.project.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -19,12 +20,12 @@ public class MemberService {
         }
         return memberId;
     }
-
-    public void create(String loginId, String email){
+    @Transactional
+    public Long create(String loginId){
         Member member = Member.builder()
                 .loginId(loginId)
-                .email(email)
                 .build();
-        memberRepository.save(member);
+        Member m = memberRepository.save(member);
+        return m.getId();
     }
 }
