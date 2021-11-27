9301fca8daf77e48309a1aeb64c64e66334a65e3
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-28 02:38:16 +0900

    fix: post detail response, post simple response로 구분

--- src/main/java/com/yapp/project/post/dto/response/PostDetailResponse.java
+++ src/main/java/com/yapp/project/post/dto/response/PostDetailResponse.java
@@ -3,6 +3,7 @@ package com.yapp.project.post.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,8 +33,6 @@ public class PostInfoResponse {
 
     private final String status;
 
-    private final Long ownerId;
-
     private final String onlineInfo;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
@@ -42,5 +41,16 @@ public class PostInfoResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private final LocalDateTime modifiedAt;
 
-    private final List<RecruitingStatusResponse> recruitingStatusResponses;
+    private final MemberDto leader;
+
+    @RequiredArgsConstructor
+    public static class MemberDto{
+        private final Long leaderId;
+
+        private final String nickName;
+
+        private final String profileImageUrl;
+
+        private final String address;
+    }
 }
---
+++ src/main/java/com/yapp/project/post/dto/response/PostSimpleResponse.java
@@ -0,0 +1,34 @@
+package com.yapp.project.post.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class PostSimpleResponse {
+    private final Long postId;
+
+    private final List<String> imageUrls;
+
+    private final String title;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime endDate;
+
+    private final String region;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime modifiedAt;
+
+    private List<String> positions;
+}
