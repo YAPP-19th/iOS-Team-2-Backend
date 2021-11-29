1a575753c384e6fc0b1483238822696583bbf583
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-30 00:15:08 +0900

    feat: apply request dto 생성

---
+++ src/main/java/com/yapp/project/apply/dto/request/ApplyRequest.java
@@ -0,0 +1,10 @@
+package com.yapp.project.apply.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApplyRequest {
+    private Long recruitingPositionId;
+}
