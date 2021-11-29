e064f7a2cd7840b891cbb250c29a6a0816e58345
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-30 00:14:50 +0900

    feat: 사용한 메소드 및 쿼리 메소드 명시

--- src/main/java/com/yapp/project/apply/repository/ApplyRepository.java
+++ src/main/java/com/yapp/project/apply/repository/ApplyRepository.java
@@ -1,7 +1,17 @@
 package com.yapp.project.apply.repository;
 
 import com.yapp.project.apply.entity.Apply;
+import com.yapp.project.post.entity.RecruitingPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+    @Override
+    boolean existsById(Long id);
+
+    @Override
+    Optional<Apply> findById(Long id);
+
+    long countByRecruitingPosition(RecruitingPosition recruitingPosition);
 }
