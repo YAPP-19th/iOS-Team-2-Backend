2cb0c507f259d76db8fc37b71ee7204af74597f9
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-30 12:59:12 +0900

    chore: 사용한 메소드 명시

--- src/main/java/com/yapp/project/apply/repository/ApplyRepository.java
+++ src/main/java/com/yapp/project/apply/repository/ApplyRepository.java
@@ -1,9 +1,11 @@
 package com.yapp.project.apply.repository;
 
 import com.yapp.project.apply.entity.Apply;
+import com.yapp.project.post.entity.Post;
 import com.yapp.project.post.entity.RecruitingPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
@@ -13,5 +15,9 @@ public interface ApplyRepository extends JpaRepository<Apply, Long> {
     @Override
     Optional<Apply> findById(Long id);
 
+    List<Apply> findAllByPost(Post post);
+
     long countByRecruitingPosition(RecruitingPosition recruitingPosition);
+
+    long countByRecruitingPositionAAndApplyStatusCode(RecruitingPosition recruitingPosition, int applyStatusCode);
 }
