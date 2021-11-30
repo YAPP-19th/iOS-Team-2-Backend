be556d82108c6487b9a86be1abfca3561016c397
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-01 03:10:50 +0900

    feat: 좋아한 멤버 리스트 반환 로직 구현

--- src/main/java/com/yapp/project/member/service/LikeMemberService.java
+++ src/main/java/com/yapp/project/member/service/LikeMemberService.java
@@ -3,6 +3,8 @@ package com.yapp.project.member.service;
 import com.yapp.project.common.exception.ExceptionMessage;
 import com.yapp.project.common.exception.type.IllegalRequestException;
 import com.yapp.project.common.exception.type.NotFoundException;
+import com.yapp.project.common.value.Position;
+import com.yapp.project.member.dto.LikeMemberResponse;
 import com.yapp.project.member.entity.LikeMember;
 import com.yapp.project.member.entity.Member;
 import com.yapp.project.member.repository.LikeMemberRepositroy;
@@ -10,6 +12,8 @@ import com.yapp.project.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,5 +49,28 @@ public class LikeMemberService {
 
         LikeMember likeMember = new LikeMember(fromMember, toMember);
         likeMemberRepositroy.delete(likeMember);
+    }
+
+    public LikeMemberResponse findAll(Long fromMemberId) {
+        Member fromMember = memberRepository.findById(fromMemberId)
+                .orElseThrow(() -> new NotFoundException(ExceptionMessage.NOT_EXIST_MEMBER_ID));
+
+        List<LikeMember> likeMembers = likeMemberRepositroy.findAllByFromMember(fromMember);
+
+        LikeMemberResponse response = new LikeMemberResponse();
+        for(var likeMember : likeMembers){
+            var toMember = likeMember.getToMember();
+            var memberInfo = new LikeMemberResponse.LikedMember(
+                    toMember.getId(),
+                    toMember.getNickName(),
+                    toMember.getProfileImageUrl(),
+                    toMember.getAddress(),
+                    Position.of(toMember.getPositionCode()).getPositionName()
+            );
+
+            response.getLikedMembers().add(memberInfo);
+        }
+
+        return response;
     }
 }
