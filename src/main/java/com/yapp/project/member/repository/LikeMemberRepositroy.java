1dc677dfadc033623d0a826ec1b5c14c637cfa2a
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-01 02:52:20 +0900

    feat: 사용한 메소드 명시

--- src/main/java/com/yapp/project/member/repository/LikeMemberRepositroy.java
+++ src/main/java/com/yapp/project/member/repository/LikeMemberRepositroy.java
@@ -1,7 +1,15 @@
 package com.yapp.project.member.repository;
 
 import com.yapp.project.member.entity.LikeMember;
+import com.yapp.project.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeMemberRepositroy extends JpaRepository<LikeMember, Long> {
+    @Override
+    <S extends LikeMember> S save(S entity);
+
+    @Override
+    void delete(LikeMember entity);
+
+    boolean existsByFromMemberAndToMember(Member fromMember, Member toMember);
 }
