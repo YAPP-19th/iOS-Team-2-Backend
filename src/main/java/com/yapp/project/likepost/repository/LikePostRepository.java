f8b6f34c75f661da475273b552cd67ee2338f350
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-01 20:41:40 +0900

    chore: 사용한 메소드 명시

--- src/main/java/com/yapp/project/likepost/repository/LikePostRepository.java
+++ src/main/java/com/yapp/project/likepost/repository/LikePostRepository.java
@@ -1,7 +1,20 @@
 package com.yapp.project.likepost.repository;
 
 import com.yapp.project.likepost.entity.LikePost;
+import com.yapp.project.member.entity.Member;
+import com.yapp.project.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LikePostRepository extends JpaRepository<LikePost, Long> {
+    @Override
+    <S extends LikePost> S save(S entity);
+
+    boolean existsByMemberAndPost(Member member, Post post);
+
+    Optional<LikePost> findByMemberAndPost(Member member, Post post);
+
+    List<LikePost> findAllByMember(Member member);
 }
