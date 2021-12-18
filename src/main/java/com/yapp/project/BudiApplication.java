7965e3b53715c2d3bba2afa570d6f2b80982f5b6
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-18 16:00:26 +0900

    feat: 스프링 부트 실행 시간 서울시간으로 고정

--- src/main/java/com/yapp/project/BudiApplication.java
+++ src/main/java/com/yapp/project/BudiApplication.java
@@ -4,8 +4,16 @@ import com.yapp.project.config.ApplicationConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BudiApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(BudiApplication.class)
