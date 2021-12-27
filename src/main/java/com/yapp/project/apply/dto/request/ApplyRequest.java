9f0b4b0ee41c5cdb5d448a1c02de48d308e00b7d
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-27 16:16:54 +0900

    feat: apply request dto validation 적용

--- src/main/java/com/yapp/project/apply/dto/request/ApplyRequest.java
+++ src/main/java/com/yapp/project/apply/dto/request/ApplyRequest.java
@@ -1,12 +1,20 @@
 package com.yapp.project.apply.dto.request;
 
+import com.yapp.project.common.exception.DtoValidationFailMessage;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import javax.validation.constraints.Min;
 
 @Getter
 @AllArgsConstructor
 public class ApplyRequest {
+    @ApiModelProperty(example = "1")
+    @Min(value = 1L, message = DtoValidationFailMessage.INVALID_POST_ID)
     private Long postId;
 
+    @ApiModelProperty(example = "1")
+    @Min(value = 1L, message = DtoValidationFailMessage.INVALID_RECRUITINGPOSITION_ID)
     private Long recruitingPositionId;
 }
--- src/main/java/com/yapp/project/common/exception/DtoValidationFailMessage.java
+++ src/main/java/com/yapp/project/common/exception/DtoValidationFailMessage.java
@@ -5,19 +5,16 @@ public class DtoValidationFailMessage {  //TODO: 메모리 효율적인 방안�
     public final static String INVALID_CATEGORY = "INVALID_CATEGORY";
     public final static String INVALID_REGION = "INVALID_REGION";
     public final static String INVALID_DESCRIPTION = "INVALID_DESCRIPTION";
-    public final static String INVALID_OWNER_ID = "INVALID_OWNER_ID";
     public final static String INVALID_RECRUITING_NUMBER_NUMBER = "INVALID_RECRUITING_NUMBER";
     public final static String INVALID_POSITION_NAME = "INVALID_POSITION_NAME";
-    public final static String INVALID_SKILL_NAME = "INVALID_SKILL_NAME";
     public final static String INVALID_POST_ONLINE_INFO = "INVALID_POST_ONLINE_INFO";
-    public final static String EMPTY_ACCESS_TOKEN = "EMPTY_ACCESS_TOKEN";
     public final static String EMPTY_POST_IMAGE_URL = "EMPTY_POST_IMAGE_URL";
     public final static String INVALID_TIME = "INVALID_TIME";
-    public final static String INVALID_TARGET_MEMBER = "INVALID_TARGET_MEMBER";
     public final static String INVALID_POST_ID = "INVALID_POST_ID";
     public final static String EMPTY_SELECTED_REVIEW = "EMPTY_SELECTED_REVIEW";
     public final static String INVALID_REVIEW_TITLE = "INVALID_REVIEW_TITLE";
     public final static String INVALID_REVIEW_CONTENT = "INVALID_REVIEW_CONTENT";
     public final static String INVALID_NICKNAME = "INVALID_NICKNAME";
     public final static String INVALID_ADDRESS = "INVALID_ADDRESS";
+    public final static String INVALID_RECRUITINGPOSITION_ID = "INVALID_RECRUITINGPOSITION_ID";
 }
