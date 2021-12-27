65bd55c050836b26ec1ce86cd3d04306188da62a
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-27 17:28:56 +0900

    chore: review dto validation 수정 (content 길이)

--- src/main/java/com/yapp/project/review/dto/request/TextReviewCreateRequest.java
+++ src/main/java/com/yapp/project/review/dto/request/TextReviewCreateRequest.java
@@ -12,6 +12,6 @@ public class TextReviewCreateRequest {
     @Size(min = 2, max = 255, message = DtoValidationFailMessage.INVALID_REVIEW_TITLE)
     private String title;
 
-    @Size(min = 2, max = 255, message = DtoValidationFailMessage.INVALID_REVIEW_CONTENT)
+    @Size(min = 2, max = 1000, message = DtoValidationFailMessage.INVALID_REVIEW_CONTENT)
     private String content;
 }
