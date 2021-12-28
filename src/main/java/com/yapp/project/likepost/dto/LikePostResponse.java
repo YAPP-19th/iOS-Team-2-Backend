138f02dd0e2c8fb8ea43d34d4d4d7cc1958ea610
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-29 04:45:29 +0900

    feat: like post에 대해 페이징 처리 추가

--- src/main/java/com/yapp/project/likepost/dto/LikePostResponse.java
+++ src/main/java/com/yapp/project/likepost/dto/LikePostResponse.java
@@ -1,35 +1,25 @@
 package com.yapp.project.likepost.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
 public class LikePostResponse {
-    private List<LikedPost> likedPosts;
+    private final Long postId;
 
-    @Getter
-    @RequiredArgsConstructor
-    public static class LikedPost{
-        private final Long postId;
+    private final String mainImageUrl;
 
-        private final String mainImageUrl;
+    private final String title;
 
-        private final String title;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime startDate;
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        private final LocalDateTime startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime endDate;
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        private final LocalDateTime endDate;
-
-        private final String projectStatus;
-    }
+    private final String projectStatus;
 }
