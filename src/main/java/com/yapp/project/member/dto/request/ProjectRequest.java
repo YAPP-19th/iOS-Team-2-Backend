e0f874933b88d33ac4c68e9d2673afddcbb047d1
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-12-30 14:59:32 +0900

    refactor: dto validation 추가

--- src/main/java/com/yapp/project/member/dto/request/ProjectRequest.java
+++ src/main/java/com/yapp/project/member/dto/request/ProjectRequest.java
@@ -1,14 +1,17 @@
 package com.yapp.project.member.dto.request;
 
+import com.yapp.project.common.exception.DtoValidationFailMessage;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Getter
 @Builder
 public class ProjectRequest {
+    @NotBlank(message = DtoValidationFailMessage.INVALID_PROJECT_TITLE)
     private String name;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -17,6 +20,7 @@ public class ProjectRequest {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDate endDate;
 
+    @NotBlank(message = DtoValidationFailMessage.INVALID_DESCRIPTION)
     private String description;
 
 }
