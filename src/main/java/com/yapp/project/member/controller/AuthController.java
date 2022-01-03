f15287a88ce1807d7da4a137671d8ffc6ada51ed
Jihun-Hwang <goharrm@ajou.ac.kr>
2022-01-03 19:59:25 +0900

    feat: member entity에 fcm_token 추가

--- src/main/java/com/yapp/project/member/controller/AuthController.java
+++ src/main/java/com/yapp/project/member/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.yapp.project.member.controller;
 
 import com.yapp.project.common.web.ApiResult;
 import com.yapp.project.common.web.ResponseMessage;
+import com.yapp.project.member.dto.request.FcmTokenRequest;
 import com.yapp.project.member.dto.request.LoginRequest;
 import com.yapp.project.member.dto.response.LoginResponse;
 import com.yapp.project.member.service.JwtService;
@@ -13,11 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/auth")
-@Api(tags = "로그인")
+@Api(tags = "인증설정")
 public class AuthController {
     private final MemberService memberService;
     private final JwtService jwtService;
@@ -33,6 +35,23 @@ public class AuthController {
         response = jwtService.loginResponse(memberId, request.getLoginId());
         return ResponseEntity.ok(
                 ApiResult.of(ResponseMessage.LOGIN_SUCCESS, response)
+        );
+    }
+
+    @ApiOperation(value = "FCM 토큰 저장", notes = "알림 비활성화를 하더라도 fcm 토큰은 서버에 전송")
+    @PutMapping(value = "/fcms")
+    public ResponseEntity<ApiResult> updateFcmToken(
+            @Valid @RequestBody FcmTokenRequest request,
+            @RequestHeader("accessToken") @NotBlank String accessToken
+    ) {
+
+        jwtService.validateTokenForm(accessToken);
+        long currentMemberId = jwtService.getMemberId(accessToken);
+
+        memberService.updateFcmToken(currentMemberId, request.getFcmToken(), request.getIsFcmTokenActive());
+
+        return ResponseEntity.ok(
+                ApiResult.of(ResponseMessage.SUCCESS)
         );
     }
 }
